c7a6a11eb50c0ab37f46327fd75da562ef670827
ImNatHarvey <jharveycrisologo@gmail.com>
2025-11-12 20:06:26 +0800

    Refactored customer controller

---
+++ src/main/java/com/toastedsiopao/controller/CustomerOrderController.java
@@ -0,0 +1,29 @@
+package com.toastedsiopao.controller;
+
+import com.toastedsiopao.model.SiteSettings;
+import com.toastedsiopao.service.SiteSettingsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/u")
+public class CustomerOrderController {
+
+	@Autowired
+	private SiteSettingsService siteSettingsService;
+
+	@ModelAttribute
+	public void addCommonAttributes(Model model) {
+		SiteSettings settings = siteSettingsService.getSiteSettings();
+		model.addAttribute("siteSettings", settings);
+	}
+
+	@GetMapping("/order")
+	public String customerOrder() {
+		return "customer/order";
+	}
+}
